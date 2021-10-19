2a4fc069834614443b31c8e937854dcb61e95a54
Michala Nybroe <michalanim@gmail.com>
2021-10-19 16:07:56 +0200

    controller index

--- src/main/java/login/controller/MyController.java
+++ src/main/java/login/controller/MyController.java
@@ -1,4 +1,11 @@
 package login.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class MyController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
